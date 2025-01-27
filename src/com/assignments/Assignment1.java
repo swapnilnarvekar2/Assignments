749cdbc7abebaff92d131070f856c8d8ab5afd55
Narvekar <swapnil.narvekar@allscripts.com>
2025-01-27 18:48:44 +0530

    This is data type assignment

---
+++ src/com/assignments/Assignment1.java
@@ -0,0 +1,69 @@
+package com.assignments;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//1. Temprature of a city in degree celcius
+		float temprature = 25.5f;
+		
+		//2. Whether a customer has placed an order?
+		boolean isOrderPlaced = true;
+		
+		//3. Person's phone number
+		long phoneNumber = 123-456-7890l;
+		
+		//4. Amount of money in a customer's bank account
+		float bankBalance = 1000.50f;
+		
+		//5. persons email address
+		String emailAddress = "john.doe@example.com";
+		
+		//6. Coordinates of a location
+		double coordinates = -122.4194;
+		
+		//7. Persons maritial status
+		boolean isMarried = false;
+		
+		//8. Persons occupation
+		String occupation = "Software Engineer";
+		
+		//9. Persons favourite colour
+		String colour = "Orange";
+		
+		//10. Current year
+		short year = 2025;
+		
+		//11. Number of followers on social media
+		int followers = 100000;
+		
+		//12. Rating of movie
+		float rating = 7.5f;
+		
+		//13. Persons blood type
+		char bloodType = 'A';
+		
+		//14. Title of book
+		String bookTitle = "To kill a Mockingbird";
+		
+		//15. Number of employees in a company
+		short employeeCount = 500;
+		
+		//16. Time of an event
+		String time = "2:30 PM";
+		
+		//17. Name of a country
+		String country = "United States";
+		
+		//18. Persons eye colour
+		String eyeColor = "Brown";
+		
+		//19. Persons birthplace
+		String city = "New York City";
+		
+		//20. Distance between two cities
+		float distance = 200.5f;
+	}
+
+}
